c80dd99479c34adce852656cbb67edc9d5e1116d
iamb0ttle <bhyun08@dsm.hs.kr>
2026-03-20 12:51:54 +0000

    add comments to daily core logic

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ import Layout from "./components/Layout"
 export default function App() {
     return (
         <Routes>
+            {/* Routes는 react-router-dom 라이브러리에서 제공하는 컴포넌트로, Routes를 등록하고 관리할 수 있도록 한다. */}
             <Route element={<Layout/>}>
-                {/* Routes는 react-router-dom 라이브러리에서 제공하는 컴포넌트로, Routes를 등록하고 관리할 수 있도록 한다. */}
+                {/* Layout 컴포넌트를 사용하여 공통 레이아웃을 적용한다. */}
                 <Route path="/" element={<Home />} />
                 {/* Route는 react-router-dom 라이브러리에서 제공하는 컴포넌트로 특정 URL 경로와 컴포넌트를 연결한다. */}
                 <Route path="/about" element={<About />} />
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,10 +7,12 @@ export default function Layout() {
                 <div className="appTitle">Study Hub</div>
 
                 <nav className="appNav">
-                    <NavLink
+                    {/* NavLink는 react-router-dom 라이브러리에서 제공하는 컴포넌트로, 페이지 간 이동을 구현한다. */}
+                    <NavLink 
                     to={"/"}
                     end
                     className={({isActive}) => (isActive ? "navLink active" : "navLink")}
+                    /* isActive를 통해 NavLink 상태에 따른 클래스 이름을 설정한다. */
                     >
                         Home   
                     </NavLink>
@@ -53,6 +55,8 @@ export default function Layout() {
             </header>
 
             <main className="appMain">
+                {/* Outlet는 react-router-dom 라이브러리에서 제공하는 컴포넌트로, 현재 페이지의 하위 컴포넌트를 렌더링한다. 
+                자식 Route 컴포넌트가 해당 위치로 렌더링되게 된다.*/}
                 <Outlet/>
             </main>
         </div>
